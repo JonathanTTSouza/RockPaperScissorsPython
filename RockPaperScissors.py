eaa1a5af80667e3eda2364dd551729d8262b0456
JonathanTTSouza <68395424+JonathanTTSouza@users.noreply.github.com>
2021-05-27 20:50:11 -0300

    Formatting

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -18,8 +18,7 @@ while True:
         continue
 
     # randomize computer choice
-    computer_choice = ['rock', 'paper', 'scissors']
-    computer_choice = random.choice(computer_choice)
+    computer_choice = random.choice(['rock', 'paper', 'scissors'])
 
     # draw check
     if (choice == 'rock' and computer_choice == 'rock') or (choice == 'paper' and computer_choice == 'paper') or (choice == 'scissors' and computer_choice == 'scissors'):
@@ -27,11 +26,11 @@ while True:
         print('The computer chose: {}\nDraw!'.format(computer_choice))
 
     # player wins check
-    elif (choice == 'rock' and computer_choice == 'scissors') or (choice == 'scissors' and computer_choice == 'paper') or (choice == 'paper' and computer_choice == 'rock'):
+    elif (choice == 'rock' and computer_choice == 'scissors') or (choice == 'paper' and computer_choice == 'rock') or (choice == 'scissors' and computer_choice == 'paper'):
         print('You chose {}'.format(choice))
         print('The computer chose: {}\nYou win!'.format(computer_choice))
 
     # player loses check
-    elif (choice == 'rock' and computer_choice == 'paper') or (choice == 'scissors' and computer_choice == 'rock') or (choice == 'paper' and computer_choice == 'scissors'):
+    elif (choice == 'rock' and computer_choice == 'paper') or (choice == 'paper' and computer_choice == 'scissors') or (choice == 'scissors' and computer_choice == 'rock'):
         print('You chose {}'.format(choice))
         print('The computer chose: {}\nYou lose!'.format(computer_choice))
